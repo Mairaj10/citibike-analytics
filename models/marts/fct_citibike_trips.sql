c921fffda00bdccf2f4884ec977a36ecf95b9359
Mairaj <rashdimairaj9@gmail.com>
2026-08-20 16:57:03 +0500

    Implement incremental Citi Bike trip fact model

---
+++ models/marts/fct_citibike_trips.sql
@@ -0,0 +1,35 @@
+{{
+    config(
+        materialized='incremental',
+        unique_key='ride_id',
+        incremental_strategy='merge'
+    )
+}}
+
+select
+    ride_id,
+    rideable_type,
+    started_at,
+    ended_at,
+    start_station_name,
+    start_station_id,
+    end_station_name,
+    end_station_id,
+    start_lat,
+    start_lng,
+    end_lat,
+    end_lng,
+    member_casual,
+    source_filename,
+    loaded_at
+
+from {{ ref('stg_citibike_trips') }}
+
+{% if is_incremental() %}
+
+where loaded_at > (
+    select max(loaded_at)
+    from {{ this }}
+)
+
+{% endif %}
